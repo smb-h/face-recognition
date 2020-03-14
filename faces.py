c945ce0278f60343a1bf594f766e9f0c3e23d30e
smbh <smb_h@yahoo.com>
2020-03-14 19:56:46 +0330

    minor changes

---
+++ faces.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import pickle
+
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+
+labels = {}
+# DeSerialize label id's
+with open("labels.pickle", 'rb') as f:
+    org_labels = pickle.load(f)
+    labels = {v:k for k,v in org_labels.items()}
+
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        # print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        white_color = (255, 255, 255)
+        stroke = 2
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 85:
+            name = labels[id_]
+            cv2.putText(frame, name, (x, y), font, 1, white_color, stroke, cv2.LINE_AA)
+
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_color)
+
+        # Draw rectangle
+        rec_color = (255, 30, 30) # BGR as blue green red
+        rec_stroke = 2
+        cv2.rectangle(frame, (x, y), (x+w, y+h), rec_color, rec_stroke)
+        # detect eyes
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        # for (ex, ey, ew, eh)in eyes:
+        #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        # detect smile
+        # smiles = smile_cascade.detectMultiScale(roi_gray)
+        # for (sx, sy, sw, sh)in smiles:
+        #     cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ faces_train.py
@@ -0,0 +1,71 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+images_dir = os.path.join(BASE_DIR, "images")
+
+y_labels = []
+x_train = []
+
+current_id = 0
+label_ids = {}
+
+
+for root, dirs, files in os.walk(images_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            # label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
+            label = os.path.basename(root).replace(" ", "-").lower()
+
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
+
+
+            # some number
+            # y_labels.append(label)
+            # verify this image, turn into a NUMPY array, GRAY
+            # x_train.append(path)
+
+            # open image and convert it to grayscale
+            pil_image = Image.open(path).convert("L")
+            # resize image
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+
+
+            image_array = np.array(pil_image, "uint8")
+
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_)
+
+
+# print(y_labels)
+# print(x_train)
+
+
+# serialize label id's
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+# train
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
+
+
+# e4 min 44
